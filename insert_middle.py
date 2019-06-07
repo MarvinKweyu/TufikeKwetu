9e9629695eb5b96f3952588b70fbde545beb4123
MarvinKweyu <mkweyu1@gmail.com>
2019-06-07 15:38:50 +0300

    Removed unnecessary files.Added folders for different sections of the output.

--- insert_middle.py
+++
@@ -1,7 +0,0 @@
-import fileinput
-
-file_path = 'sample.html'
-for line in fileinput.FileInput(file_path, inplace=1):
-    if "</body>" in line:
-        line = line.replace(line, "NEW_TEXT\n"+line)
-    print (line)
