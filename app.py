22d1979d60330ac9d01cf47acd557ceca150b30c
MarvinKweyu <mkweyu1@gmail.com>
2019-12-09 21:12:15 +0300

    Create hover tool
    Add details to gitignore file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models.sources import ColumnDataSource
+from bokeh.models import HoverTool
 
 
 app = Flask(__name__)
@@ -21,14 +22,25 @@ Items to generate:
 """
 items_for_compare = ['Year', 'State', 'Month']
 
+
+# def create_figure(according_to,hover_tool=None):
 def create_figure(according_to):
     """Create main plot. """
     # group by category selected
     fatalities[according_to] = fatalities[according_to].astype(str)
     group = fatalities.groupby(according_to)
     source = ColumnDataSource(group)
+
+    if according_to == "Year":
+        TOOLTIPS = [
+            ('Fatalities','$y'),
+        ]
+    else:
+        TOOLTIPS = None
     
-    plot = figure(plot_height=600, plot_width=900, x_range=group, title=f"Fatalities according to {according_to}")
+
+    plot = figure(plot_height=600, plot_width=900, x_range=group,
+                  title=f"Fatalities according to {according_to}", tooltips=TOOLTIPS)
 
     if according_to == 'Year':
         plot.line(x='Year', y='Age_count', line_width=2, source=source)
@@ -39,7 +51,36 @@ def create_figure(according_to):
     
     plot.toolbar.logo = None
 
+    hover = plot.select(dict(type=HoverTool))
+    hover.mode = 'vline' # hover mode to year
+
     return plot
+
+
+def create_hover():
+    """Create a hover tool.
+    ? Dependent tooltips
+    """
+
+    hover_html = """
+    <div>
+        <span class="hover-tooltip>$x</span>
+    <div>
+    # if hover_tool:
+    #     tools = [hover_tool]
+
+     <div>
+        <span class="hover-tooltip>@Age_count fatalities</span>
+    <div>
+
+     <div>
+        <span class="hover-tooltip>$x</span>
+    <div>
+     """
+
+
+    return HoverTool(tooltips=hover_html)
+    
 
 
 @app.route('/')
@@ -49,7 +90,8 @@ def index():
     current_comparison = request.args.get("item_for_compare")
     if current_comparison == None:
         current_comparison = "Year"
-
+    
+    # hover_tool = create_hover()
     # create plot
     plot = create_figure(current_comparison)
 
